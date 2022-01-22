4e50853253cbb11d0f7d337d3c62387338c3e8a5
did <suhn0415@naver.com>
2022-01-22 17:52:15 +0900

    dd

--- Assets/Script/PlayerMove.cs
+++ Assets/Script/PlayerMove.cs
@@ -7,9 +7,9 @@ public class PlayerMove : MonoBehaviour
     float speed = 10f;
     Vector3 mousePos, transPos, targetPos;
     bool isHorizontal = true;
+
     void Start()
     {
-
     }
 
     void Update()
@@ -18,21 +18,22 @@ public class PlayerMove : MonoBehaviour
         {
             CalTargetPos();
         }
-
     }
-
+    private Vector2 tpos2d;
     void CalTargetPos()
     {
         mousePos = Input.mousePosition;
         transPos = Camera.main.ScreenToWorldPoint(mousePos);
         targetPos = new Vector3(transPos.x, transPos.y, 0);
-
     }
 
     void FixedUpdate()
     {
-        // Vector3 targetPos = isHorizontal ? new Vector3(transPos.x, 0) : new Vector3(0, transPos.y);
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, Time.deltaTime * speed); 
+    }
 
-        transform.position = Vector3.MoveTowards(transform.position, targetPos, Time.deltaTime * speed);
+    private void OnCollisionEnter2D(Collision2D collision)
+    { 
+        print(collision.collider.name);
     }
 }
