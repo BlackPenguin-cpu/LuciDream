5dce288c7a5a6d8f2e8c21aa333796677a095887
did <suhn0415@naver.com>
2022-01-24 21:47:51 +0900

    aa

---
+++ Assets/UI/MainMenu/Option.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Option : MonoBehaviour
+{
+    public AudioSource Music;
+    public Slider slider;
+    public Text text;
+    float Value = 0;
+    int p = 0;
+   
+    public void Slider_right()
+    {
+        if (Value < 1)
+        {
+            Value += 0.1f;
+            p += 10;
+            
+        }
+        text.text = p.ToString() + "%";
+        slider.GetComponent<Slider>().value = Value;
+    }
+
+    public void Slider_left()
+    {
+        if(Value > 0.1f)
+        {
+            Value -= 0.1f;
+            p -= 10;
+        }
+        text.text = p.ToString() + "%";
+        slider.GetComponent <Slider>().value = Value;
+    }
+
+    public void SetMusicVolume(float Volume)
+    {
+        Music.volume = Volume;
+    }
+}
---
+++ Assets/UI/MainMenu/onClick_MainMenu.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class onClick_MainMenu : MonoBehaviour
+{
+    public GameObject MainMenu;
+    public GameObject Opition;
+    public GameObject Exit;
+    public GameObject RS;
+    public void Opition_in()
+    {
+        MainMenu.SetActive(false);
+        Exit.SetActive(false);
+        Opition.SetActive(true);
+        RS.SetActive(false); 
+    }
+    public void Opition_out()
+    {
+        MainMenu.SetActive(true);
+        Exit.SetActive(false);
+        Opition.SetActive(false);
+        RS.SetActive(false);
+    }
+    public void Exit_in()
+    {
+        MainMenu.SetActive(false);
+        Exit.SetActive(true);
+        Opition.SetActive(false);
+        RS.SetActive(false);
+    }
+    public void Exit_out()
+    {
+        MainMenu.SetActive(true);
+        Exit.SetActive(false);
+        Opition.SetActive(false);
+        RS.SetActive(false);
+    }
+    public void RS_in()
+    {
+        MainMenu.SetActive(false);
+        Exit.SetActive(false);
+        Opition.SetActive(false);
+        RS.SetActive(true);
+    }
+    public void RS_out()
+    {
+        MainMenu.SetActive(false);
+        Exit.SetActive(false);
+        Opition.SetActive(true);
+        RS.SetActive(false);
+    }
+    public void OnApplicationQuit()
+    {
+        Application.Quit();
+    }
+    public void StartGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
