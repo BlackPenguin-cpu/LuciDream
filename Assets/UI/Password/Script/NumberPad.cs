80b11da130ef4eda0f72ab249abd8647d984db66
limjiho <ljiho1208@gmail.com>
2022-01-30 17:07:33 +0900

    연출 대충 완성

--- Assets/UI/Password/Script/NumberPad.cs
+++ Assets/UI/Password/Script/NumberPad.cs
@@ -2,11 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
 
 public class NumberPad : MonoBehaviour
 {
     public TextMeshProUGUI NumberBoard;
+    public GameObject numberpad;
+    public Image BackGround;
     public string Password;
+    private bool AnswerCheck = true;
     void Start()
     {
     }
@@ -29,21 +34,64 @@ public class NumberPad : MonoBehaviour
         if (NumberBoard.text.Length != 0)
         {
             NumberBoard.text = NumberBoard.text.Substring(0, NumberBoard.text.Length - 1);
+            AnswerCheck = true;
         }
     }
 
     void PasswordCheck()
     {
-        if (NumberBoard.text.Length == 4)
+        if (NumberBoard.text.Length == 4 && AnswerCheck)
         {
-        if (NumberBoard.text == Password)
-        {
-            Debug.Log("맞음!!");
-        }
-        else
-        {
-            Debug.Log("틀림!!");
-        }
+            AnswerCheck = false;
+            if (NumberBoard.text == Password)
+            {
+                Debug.Log("맞음!!");
+                StartCoroutine(ClearDirecting());
+            }
+            else
+            {
+                Debug.Log("틀림!!");
+                StartCoroutine(OverDireCting());
+            }
         }
     }
+
+    public void CloseButtonClick()
+    {
+        numberpad.transform.DOMove(new Vector3(960, 1540, 0), 1).SetEase(Ease.Linear);
+        NumberBoard.text = "";
+        AnswerCheck = true;
+    }
+
+    public void MoveButtonClick()
+    {
+        numberpad.transform.DOMove(new Vector3(960, 540, 0), 1).SetEase(Ease.Linear);
+    }
+
+    IEnumerator ClearDirecting()
+    {
+        BackGround.color = new Color(0, 255, 0, 0);
+        WaitForSeconds Wait = new WaitForSeconds(0.01f);
+        Color Green = new Color(0, 1 * Time.deltaTime, 0, 1 * Time.deltaTime);
+        while (BackGround.color.a <= 0.4f)
+        {
+            BackGround.color += Green;
+            yield return Wait;
+        }
+        BackGround.color = new Color(0, 0, 0, 0);
+    }
+
+    IEnumerator OverDireCting()
+    {
+        BackGround.color = new Color(255, 0, 0, 0);
+        WaitForSeconds Wait = new WaitForSeconds(0.01f);
+        Color Red = new Color(0, 0, 0, 1 * Time.deltaTime);
+        while (BackGround.color.a <= 0.4f)
+        {
+            BackGround.color += Red;
+            yield return Wait;
+        }
+        BackGround.color = new Color(0, 0, 0, 0);
+    }
 }
+
