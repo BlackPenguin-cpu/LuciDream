1fce6198b9b5756bfa7f719f9c0d122a95c42fb0
Black_Penguin <dkstmdwnswns@gmail.com>
2022-02-01 20:53:32 +0900

    Player애니매이샨샨

--- Assets/Script/AStarTest.cs
+++ Assets/Script/AStarTest.cs
@@ -29,7 +29,6 @@ public class AStarTest : Singleton<AStarTest>
 
     private void Start()
     {
-        PathFinding();
     }
     private void Update()
     {
--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -1,27 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
 using DG.Tweening;
 
 public enum PlayerState
 {
     IDLE,
     WALK,
-    DIE
+    DIE,
+    INTERACTIVE
+}
+public enum PlayerDir
+{
+    UP,
+    DOWN,
+    RIGHT,
+    LEFT
 }
 
 public class Player : Singleton<Player>
 {
     public float MoveSpeed;
     public PlayerState State;
+    public PlayerDir PlayerDir;
+    public bool Dead;
+
     private Coroutine OnMouseClickIEnumerator;
-    [SerializeField] Camera Camera;
-    public List<Node> NodeList;
+    private Camera Camera;
+    private List<Node> NodeList;
     private Tweener tween;
+    private Animator anim;
 
     private void OnLevelWasLoaded(int level)
     {
+        anim = GetComponent<Animator>();
         Camera = Camera.main;
     }
     private void Start()
@@ -31,6 +43,7 @@ public class Player : Singleton<Player>
 
     private void Update()
     {
+        AnimationChecker();
         if (Input.GetMouseButtonDown(1))
         {
             if (OnMouseClickIEnumerator != null) StopCoroutine(OnMouseClickIEnumerator);
@@ -39,9 +52,43 @@ public class Player : Singleton<Player>
         CameraMove();
     }
 
+    void AnimationChecker()
+    {
+        //anim.SetInteger("State", (int)State);
+        //anim.SetInteger("Dir", (int)State);
+
+        switch (State)
+        {
+            case PlayerState.IDLE:
+                break;
+            case PlayerState.WALK:
+                switch (PlayerDir)
+                {
+                    case PlayerDir.UP:
+                        break;
+                    case PlayerDir.DOWN:
+                        break;
+                    case PlayerDir.RIGHT:
+                        break;
+                    case PlayerDir.LEFT:
+                        break;
+                    default:
+                        break;
+                }
+                break;
+            case PlayerState.DIE:
+                Dead = true;
+                break;
+            case PlayerState.INTERACTIVE:
+                break;
+            default:
+                break;
+        }
+    }
+
     void CameraMove()
     {
-        Camera.transform.position = Vector3.Lerp(Camera.transform.position, this.gameObject.transform.position + new Vector3(0,0,-10), Time.deltaTime);
+        Camera.transform.position = Vector3.Lerp(Camera.transform.position, this.gameObject.transform.position + new Vector3(0, 0, -10), Time.deltaTime);
     }
 
     IEnumerator OnMouseClick()
@@ -50,17 +97,42 @@ public class Player : Singleton<Player>
         AStarTest.Instance.targetPos = Vector2Int.RoundToInt(mouse);
         AStarTest.Instance.PathFinding();
         NodeList = AStarTest.Instance.FinalNodeList;
-        if (NodeList.Count > 0)
+        if (NodeList.Count > 1)
         {
             foreach (Node node in NodeList)
             {
-                Debug.Log("아라라라");
+                State = PlayerState.WALK;
                 if (tween != null) tween.Kill();
-                tween = transform.DOMove(new Vector2(node.x, node.y), MoveSpeed);
+                Vector2 dir = new Vector2(node.x, node.y);
+                tween = transform.DOMove(dir, MoveSpeed);
+                VectorStateChecker(dir);
                 yield return tween;
                 yield return new WaitForSeconds(MoveSpeed);
+                State = PlayerState.IDLE;
             }
         }
         else StopCoroutine(OnMouseClickIEnumerator);
+
+    }
+
+    void VectorStateChecker(Vector2 dir)
+    {
+        if (transform.position.x < dir.x)
+        {
+            PlayerDir = PlayerDir.RIGHT;
+        }
+        else if (transform.position.x < dir.x)
+        {
+            PlayerDir = PlayerDir.LEFT;
+        }
+        else if (transform.position.y > dir.y)
+        {
+            PlayerDir = PlayerDir.DOWN;
+        }
+        else if (transform.position.y < dir.y)
+        {
+            PlayerDir = PlayerDir.UP;
+        }
     }
 }
+
