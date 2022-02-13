b9d4a1d61ee45dcce77f938851cedad37912b127
limjiho <ljiho1208@gmail.com>
2022-02-13 15:26:26 +0900

    타일맵 수정 / 롤맵 / 선풍기

--- Assets/Script/chikenMove.cs
+++ Assets/Script/chikenMove.cs
@@ -7,7 +7,7 @@ public class chikenMove : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
