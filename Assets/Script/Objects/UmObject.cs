82f8be1dc50147ba9ae2c7b744d875871ae62dac
limjiho <ljiho1208@gmail.com>
2022-02-19 10:33:35 +0900

    엄

--- Assets/Script/Objects/UmObject.cs
+++ Assets/Script/Objects/UmObject.cs
@@ -5,9 +5,18 @@ using UnityEngine;
 public class UmObject : Objects
 {
     public Sprite um2;
+    public int number;
+    public string text;
     public override void Interaction()
     {
         base.Interaction();
         GetComponent<SpriteRenderer>().sprite = um2;
+        Invoke(nameof(Die), 3f);
+        Player.Instance._State = PlayerState.DIE;
+    }
+    
+    void Die()
+    {
+        DeathManager.Instance.OnDeathUI(number, text);
     }
 }
