f0fa9f006d3aedf2d55818915b5384931aa55636
did <suhn0415@naver.com>
2022-02-19 16:03:54 +0900

    a

--- Assets/Resources/Objects/Freddy/FreddyMabb.cs
+++ Assets/Resources/Objects/Freddy/FreddyMabb.cs
@@ -8,13 +8,13 @@ public class FreddyMabb : MonoBehaviour
     public GameObject Map2;
     public GameObject Freddy;
     public GameObject blue;
+    public bool b = false;
+    public bool c = false;
     void Start()
     {
         Map2.SetActive(false);
         Freddy.SetActive(false);
     }
-
-    // Update is called once per frame
     void Update()
     {
         a += Time.deltaTime;
@@ -24,12 +24,30 @@ public class FreddyMabb : MonoBehaviour
             if(a > 6)
             {
                 Freddy.SetActive(true);
-                SoundManager.Instance.PlaySound("Freddy");
+                if(b == false)
+                {
+                    Sound();
+                }
                 blue.GetComponent<CameraWark>().VibrateDorTime(2);
             }else if(a > 8)
             {
-                DeathManager.Instance.OnDeathUI(DeathManager.Instance.DeathList[16]);
+                if(c == false)
+                {
+                    Die();
+                }
             }
         }
+    }
+
+    void Sound()
+    {
+        SoundManager.Instance.PlaySound("Freddy");
+        b = true;
+    }
+
+    void Die()
+    {
+        DeathManager.Instance.OnDeathUI(DeathManager.Instance.DeathList[16]);
+        c = true;
     }
 }
